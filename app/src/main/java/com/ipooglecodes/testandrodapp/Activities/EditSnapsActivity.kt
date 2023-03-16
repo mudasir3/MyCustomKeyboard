2d7fc1c71e97ea5c7102b41ea42e2b254800bfa3
Mudasir Mukhtar <mudasir.chohan@hotmail.com>
2023-03-16 11:30:00 +0500

    Random

--- app/src/main/java/com/ipooglecodes/testandrodapp/Activities/EditSnapsActivity.kt
+++ app/src/main/java/com/ipooglecodes/testandrodapp/Activities/EditSnapsActivity.kt
@@ -64,6 +64,7 @@ class EditSnapsActivity() : AppCompatActivity() {
 
         //registerForContextMenu(textView)
 
+
         textView.requestFocus()
         val imm: InputMethodManager = this.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
