a17107b54726948b6496c3bf78d193253a75ac39
Mudasir Mukhtar <mudasir.chohan@hotmail.com>
2023-06-26 11:30:00 +0500

    Random

--- app/src/main/java/com/ipooglecodes/testandrodapp/Activities/EditSnapsActivity.kt
+++ app/src/main/java/com/ipooglecodes/testandrodapp/Activities/EditSnapsActivity.kt
@@ -82,6 +82,7 @@ class EditSnapsActivity() : AppCompatActivity() {
 
 
 
+
         selectAllButton.setOnClickListener {
             //textView.isSelected = true
             //textView.setSelectAllOnFocus(true)
