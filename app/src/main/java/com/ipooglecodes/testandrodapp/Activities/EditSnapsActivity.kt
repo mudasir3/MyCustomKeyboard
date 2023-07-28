9a3213e898df13f3672628dbcb77b680a3bf527a
Mudasir Mukhtar <mudasir.chohan@hotmail.com>
2023-07-28 11:30:00 +0500

    Random

--- app/src/main/java/com/ipooglecodes/testandrodapp/Activities/EditSnapsActivity.kt
+++ app/src/main/java/com/ipooglecodes/testandrodapp/Activities/EditSnapsActivity.kt
@@ -80,7 +80,6 @@ class EditSnapsActivity() : AppCompatActivity() {
 
         actionBar.setDisplayHomeAsUpEnabled(true)
 
-
         selectAllButton.setOnClickListener {
             //textView.isSelected = true
             //textView.setSelectAllOnFocus(true)
