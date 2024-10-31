e3611d5a9a8690970427b835f2304ccfef83ee75
Mudasir Mukhtar <mudasir.chohan@hotmail.com>
2024-10-31 11:30:00 +0500

    Random

--- app/src/main/java/com/ipooglecodes/testandrodapp/MyInputMethodService.kt
+++ app/src/main/java/com/ipooglecodes/testandrodapp/MyInputMethodService.kt
@@ -88,7 +88,6 @@ class MyInputMethodService : InputMethodService(), KeyboardView.OnKeyboardAction
         }
 
 
-
         //Get data in shared preferences
         val textsString = pref.getString("AllSavedTexts", "?").toString()
         val pastespeed = pref.getFloat("pastespeed", 0.0F)
